d02da3379a5015e335d7639ca361cc6cdd7a9bbb
ramonzamora89 <ramonzamora89@users.noreply.github.com>
2026-06-27 17:46:06 +0000

    deploy: 5c9e4050fe98eb3696d2cdf8b30579c3b19cf517

--- _next/static/chunks/app/tecnologia/page-c6f2020d8893ed3d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[487,405,999,519,121,255],{6195:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[972,878,971,117,744],function(){return e(e.s=6195)}),_N_E=e.O()}]);
